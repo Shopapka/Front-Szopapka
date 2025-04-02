d3a70144c5a7f31d06bda45b6cafa8e8029085b3
Adam Krawczyk <kemu.krawczyk@onet.pl>
2025-04-02 10:10:37 +0200

    api calls for register, fetchfamilies and postfamily

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -6,12 +6,13 @@ import "./Dashboard.css";
 import { DUMMY_FAMILIES } from "../Dummyfiles/DUMMY.FAMILIES";
 import { animate, motion, useInView } from "motion/react";
 import { inView, stagger } from "motion";
-
+import { apiUrl } from "../../constants/url";
+import { auth } from "../../firebase";
 interface Family {
   id: Key;
   name: string;
   img: string;
-  code: string
+  code: string;
 }
 
 const Dashboard = () => {
@@ -22,20 +23,26 @@ const Dashboard = () => {
   const [newFamilyName, setNewFamilyName] = useState<string>("");
   const [newFamilyImg, setNewFamilyImg] = useState<File | null>(null);
 
-  useEffect(() => {
-    const fetchFamilies = async () => {
-      try {
-        const response = await fetch("api/jakies");
-        if (!response.ok) {
-          throw new Error("serwer krzyczy ze nie jest ok :(");
-        }
-        const data: Family[] = await response.json();
-        setFamilies(data);
-      } catch (error) {
-        console.error("nie udalo sie zlapac rodzin :(", error);
+  const fetchFamilies = async () => {
+    try {
+      const token = await auth.currentUser?.getIdToken();
+      const response = await fetch(`${apiUrl}/Family/getFamilies`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("serwer krzyczy ze nie jest ok :(");
       }
-    };
+      const data: Family[] = await response.json();
+      setFamilies(data);
+    } catch (error) {
+      console.error("nie udalo sie zlapac rodzin :(", error);
+    }
+  };
 
+  useEffect(() => {
     fetchFamilies();
   }, []);
 
@@ -59,9 +66,13 @@ const Dashboard = () => {
     try {
       const formData = new FormData();
       formData.append("image", newFamilyImg);
-
+      const token = await auth.currentUser?.getIdToken();
+      console.log(token);
       const uploadResponse = await fetch("/api/upload", {
         method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
         body: formData,
       });
 
@@ -73,20 +84,8 @@ const Dashboard = () => {
 
       const newFamily = { name: newFamilyName, img: imageUrl };
 
-      const response = await fetch("/api/rodziny", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newFamily),
-      });
+      fetchFamilies();
 
-      if (!response.ok) {
-        throw new Error("serwer krzyczy ze nie udalo sie dodac rodziny :(");
-      }
-
-      const addedFamily: Family = await response.json();
-      setFamilies([...families, addedFamily]);
       setNewFamilyName("");
       setNewFamilyImg(null);
       setIsAdding(false);
@@ -95,92 +94,107 @@ const Dashboard = () => {
     }
   };
 
-  function handleFamilyJoining()
-  {
-
+  function handleFamilyJoining() {
+    // TODO
   }
 
   return (
     <div className="dashboard_big">
       <div className="family_form_outside">
         <div className="family_form_inside">
-            
-            <motion.div className="family_form_explenations"
-            initial={{opacity: 0, x: -20}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.3}}
-            >
-              <div>
-                <h2> Jak dodać nową rodzinę? </h2>
-                <p> 1. Wybierz wspaniałą nazwę dla twojej rodziny </p>
-                <p> 2. Dodaje zdjęcie </p>
-                <p> 3. Kliknij przycisk Dodaj rodzinę </p>
-                <p className="special_p"> I już możesz cieszyć się własną rodziną 😎 To takie proste!</p>
-              </div>
-            </motion.div>
+          <motion.div
+            className="family_form_explenations"
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <div>
+              <h2> Jak dodać nową rodzinę? </h2>
+              <p> 1. Wybierz wspaniałą nazwę dla twojej rodziny </p>
+              <p> 2. Dodaje zdjęcie </p>
+              <p> 3. Kliknij przycisk Dodaj rodzinę </p>
+              <p className="special_p">
+                {" "}
+                I już możesz cieszyć się własną rodziną 😎 To takie proste!
+              </p>
+            </div>
+          </motion.div>
 
-            <motion.div className="family_form_form"
-              initial={{opacity: 0, x: 20}}
-              whileInView={{opacity: 1, x: 0}}
-              transition={{duration: 0.3}}
-            >
-              
-              <form className="form_add_family">
+          <motion.div
+            className="family_form_form"
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <form className="form_add_family">
               <label> Nazwa rodziny</label>
-                <input
-                  type="text"
-                  placeholder="Podaj nazwę rodziny"
-                  value={newFamilyName}
-                  onChange={handleNameChange}
-                  
-                />
-                <label> Wybierz Plik </label>
-                <input
-                  type="file"
-                  accept="image/*"
-                  className="form-file-input"
-                  onChange={handleFileChange}
-              
-                />
-                <button onClick={handleAddFamily}>
-                  Dodaj rodzinę
-                </button>
-
-              </form>
-            
-            </motion.div>
+              <input
+                type="text"
+                placeholder="Podaj nazwę rodziny"
+                value={newFamilyName}
+                onChange={handleNameChange}
+              />
+              <label> Wybierz Plik </label>
+              <input
+                type="file"
+                accept="image/*"
+                className="form-file-input"
+                onChange={handleFileChange}
+              />
+              <button onClick={handleAddFamily}>Dodaj rodzinę</button>
+            </form>
+          </motion.div>
         </div>
-       </div>
-  
+      </div>
+
       <div className="dashboard">
         <div className="dashboard__header">
           <p className="dashboard__label"> Twoje rodziny</p>
         </div>
 
-        <motion.div className="dashboard_join_family"
-          initial={{opacity: 0}}
-          whileInView={{opacity: 1}}
-          transition={{duration: 0.3}}
+        <motion.div
+          className="dashboard_join_family"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
         >
-          <input type="text" placeholder="Podaj Kod" id="family_code_id" className="family_input_code"/>
-          <button onClick={handleFamilyJoining} className="join_family_button">Dołącz do rodziny</button>
+          <input
+            type="text"
+            placeholder="Podaj Kod"
+            id="family_code_id"
+            className="family_input_code"
+          />
+          <button onClick={handleFamilyJoining} className="join_family_button">
+            Dołącz do rodziny
+          </button>
         </motion.div>
         <ul className="dashboard__list">
           {families.length
             ? families.map((family, index) => (
-                <motion.li className="dashboard__list-item" key={family.id}
-                  initial={{opacity: 0, x: -20}}
-                  animate={{opacity: 1, x: 0}}
-                  transition={{duration: 0.1, delay: index*0.1}}>
+                <motion.li
+                  className="dashboard__list-item"
+                  key={family.id}
+                  initial={{ opacity: 0, x: -20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.1, delay: index * 0.1 }}
+                >
                   <Card nazwa={family.name} img={family.img} id={family.id} />
                 </motion.li>
               ))
             : DUMMY_FAMILIES.map((family, index) => (
-                <motion.li className="dashboard__list-item" key={index}
-                  initial={{opacity: 0, x: -20}}
-                  animate={{opacity: 1, x: 0}}
-                  transition={{duration: 0.1, delay: index*0.1}}>
-                  <Card nazwa={family.name} img={family.img} id={family.id} code={family.code} />
+                <motion.li
+                  className="dashboard__list-item"
+                  key={index}
+                  initial={{ opacity: 0, x: -20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.1, delay: index * 0.1 }}
+                >
+                  <Card
+                    nazwa={family.name}
+                    img={family.img}
+                    id={family.id}
+                    code={family.code}
+                  />
                 </motion.li>
               ))}
         </ul>
---
+++ src/constants/url.tsx
@@ -0,0 +1,1 @@
+export const apiUrl = "https://szopapka-backend.azurewebsites.net/api";
